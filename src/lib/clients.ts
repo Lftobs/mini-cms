b1d4fcaef45be6310ab7a33b94586165a54fbd97
lftobs <tobbytobs1@gmail.com>
2025-12-04 11:52:52 +0100

    chore: add runtime env

--- src/lib/clients.ts
+++ src/lib/clients.ts
@@ -1,6 +1,21 @@
 import { hc } from "hono/client";
 import type { AppType } from "../lib";
 
-const { api } = hc<AppType>("http://localhost:4321");
+const { APP_ENV } = import.meta.env;
+let baseUrl = "http://localhost:4321";
+
+if (APP_ENV === "prod") {
+    if (!import.meta.env.BASE_URL) {
+        throw new Error("Missing baseUrl");
+    }
+    baseUrl = import.meta.env.BASE_URL;
+}
+
+
+if (!baseUrl) {
+    throw new Error("Missing baseUrl");
+}
+
+const { api } = hc<AppType>(baseUrl);
 
 export default api;

77aa6abdc972e80635bbb71387716c3fb4301bb6
lftobs <tobbytobs1@gmail.com>
2026-01-22 10:16:24 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,34 +7,38 @@ import vercel from "@astrojs/vercel";
 // import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 
-
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mini-cms-psi.vercel.app",
-    output: "server",
-    prefetch: {
-        prefetchAll: true,
-    },
-    integrations: [db(), react(), alpinejs({
-        entrypoint: "/src/entrypoint",
-    }), sitemap()],
-    // adapter: node({
-    //     mode: "standalone",
-    // }),
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
+  site: "https://mini-cms.xyz",
+  output: "server",
+  prefetch: {
+    prefetchAll: true,
+  },
+  integrations: [
+    db(),
+    react(),
+    alpinejs({
+      entrypoint: "/src/entrypoint",
     }),
-    vite: {
-        server: {
-            host: true,
-            allowedHosts: ["mini-cms-psi.vercel.app"],
-        },
+    sitemap(),
+  ],
+  // adapter: node({
+  //     mode: "standalone",
+  // }),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  vite: {
+    server: {
+      host: true,
+      allowedHosts: ["mini-cms.xyz", "mini-cms-psi.vercel.app"],
+    },
 
-        plugins: [tailwindcss()],
-    },
-    experimental: {
-        svgo: true,
-    },
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    svgo: true,
+  },
 });
